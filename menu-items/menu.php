e4f13fae4193d04b926cdcac2fccf7919ee87d55
Boopalan <118632133+Boopalan012@users.noreply.github.com>
2024-03-23 10:52:25 +0530

    code base update

---
+++ menu-items/menu.php
@@ -0,0 +1,64 @@
+<?php
+
+$menu_items = [
+    [
+        'category_eng'=>'starters',
+        'category_jpg'=>'アペタイザー',
+        'sub_category_eng'=>'Salad',
+        'sub_category_jpg'=>'サラダ',
+        'name_eng'=>'Mix Veg',
+        'name_jpg'=>'ミックスベジサラダ',
+        'price'=>'Y400',
+        'img_url'=>'',
+        'class'=>'col-lg-6'
+    ],
+    [
+        'category_eng'=>'starters',
+        'category_jpg'=>'アペタイザー',
+        'sub_category_eng'=>'Salad',
+        'sub_category_jpg'=>'サラダ',
+        'name_eng'=>'Avacado Cheese',
+        'name_jpg'=>'アボカドチーズ',
+        'price'=>'Y600',
+        'img_url'=>'',
+        'class'=>'col-lg-6'
+    ],
+    [
+        'category_eng'=>'starters',
+        'category_jpg'=>'アペタイザー',
+        'sub_category_eng'=>'Veg Side Dish',
+        'sub_category_jpg'=>'野菜サイド',
+        'name_eng'=>'Broccoli Fry',
+        'name_jpg'=>'ブロッコリーフライ',
+        'price'=>'Y600',
+        'img_url'=>'',
+        'class'=>'col-lg-4'
+    ],
+    [
+        'category_eng'=>'starters',
+        'category_jpg'=>'アペタイザー',
+        'sub_category_eng'=>'Veg Side Dish',
+        'sub_category_jpg'=>'野菜サイド',
+        'name_eng'=>'Potato Fry',
+        'name_jpg'=>'ポテトフライ',
+        'price'=>'Y350',
+        'img_url'=>'',
+        'class'=>'col-lg-4'
+    ],
+    [
+        'category_eng'=>'starters',
+        'category_jpg'=>'アペタイザー',
+        'sub_category_eng'=>'Veg Side Dish',
+        'sub_category_jpg'=>'野菜サイド',
+        'name_eng'=>'Edamame',
+        'name_jpg'=>'大豆',
+        'price'=>'Y300',
+        'img_url'=>'',
+        'class'=>'col-lg-4'
+    ],
+
+]
+
+
+
+?>
